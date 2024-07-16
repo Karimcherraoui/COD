c1d9dbd6248308a3aedf07f95565298216862f17
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-07-16 20:56:09 +0100

    Add ProductController with index and store methods

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProductService;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    protected $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
+    public function index(Request $request)
+    {
+        $products = $this->productService->getProducts($request->all());
+        return response()->json($products);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric|min:0',
+            'image' => 'nullable|image|max:2048',
+            'categories' => 'array',
+        ]);
+
+        $product = $this->productService->createProduct($validatedData);
+        return response()->json($product, 201);
+    }
+
+
+
+}
